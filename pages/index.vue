30e7830a4bc5b4347fec41e8c1c9a4318cd201d1
zayeemZaki <zayeemzaki45@gmail.com>
2025-11-17 18:46:50 -0500

    updated 127 to 122

--- pages/index.vue
+++ pages/index.vue
@@ -105,7 +105,7 @@
                 <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"/>
                 </svg>
-                <span class="text-sm font-semibold">127 minutes</span>
+                <span class="text-sm font-semibold">122 minutes</span>
               </div>
             </div>
           </div>
